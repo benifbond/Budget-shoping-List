67ad0df240adcb7737f99b9702b9dcd3d9d89ee1
robciu22 <rolegs@etik.com>
2022-06-30 20:17:40 +0200

    leaner code Homepage Part2

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,43 +1,8 @@
 
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
-import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
-import { AvatarGroup } from "@mui/material";
-
-// function Copyright(props) {
-//   return (
-//     <Typography
-//       variant="body2"
-//       color="text.secondary"
-//       align="center"
-//       {...props}
-//     >
-//       {"Copyright © "}
-//       <Link color="inherit" href="https://mui.com/">
-//         Rob & Ben
-//       </Link>{" "}
-//       {new Date().getFullYear()}
-//       {"."}
-//       <AvatarGroup>
-//         <Avatar alt="Robert" src="/static/images/avatar/1.jpg" />
-//         <Avatar alt="Beniah" src="/static/images/avatar/2.jpg" />
-//       </AvatarGroup>
-//     </Typography>
-//   );
-// }
 
 
 const theme = createTheme();
@@ -45,12 +10,6 @@ const theme = createTheme();
 export default function SignInSide() {
   const handleSubmit = (event) => {
     event.preventDefault();
-    // const data = new FormData(event.currentTarget);
-    // console.log({
-    //   user: data.get("user"),
-    //   email: data.get("email"),
-    //   password: data.get("password"),
-    // });
   };
 
   return (
@@ -73,8 +32,7 @@ export default function SignInSide() {
             backgroundPosition: "center",
           }}
         />
-        
-      </Grid>
+        </Grid>
     </ThemeProvider>
   );
 }
